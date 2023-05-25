9ab917658e4735a77c4551a3eba17069a7343098
sunmin <sunm309@naver.com>
2023-05-26 00:31:02 +0900

    initial commit

---
+++ android/app/src/main/kotlin/com/example/dasi_bom_client/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dasi_bom_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: false,
+          title: Text('다시봄'),
+        ),
+        body: SizedBox(),
+        bottomNavigationBar: BottomAppBar(),
+      ),
+    );
+  }
+}
