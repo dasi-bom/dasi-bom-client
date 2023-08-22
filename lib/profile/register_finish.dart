e2a90b866c36b461c1d55a093652258048d7bdbc
sunmin <sunm309@naver.com>
2023-08-22 21:51:58 +0900

    #25 fix: 등록 완료 페이지 내 사용자 조회 api 연동 작업중

--- lib/profile/register_finish.dart
+++ lib/profile/register_finish.dart
@@ -2,16 +2,66 @@ import 'package:confetti/confetti.dart';
 import 'package:dasi_bom_client/MainPage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import '../WritingPage.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class RegisterFinish extends StatefulWidget {
-  const RegisterFinish({Key? key}) : super(key: key);
+  const RegisterFinish({Key? key, this.data}) : super(key: key);
+  final data;
 
   @override
   State<RegisterFinish> createState() => _RegisterFinishState();
 }
 
 class _RegisterFinishState extends State<RegisterFinish> {
+  final storage = FlutterSecureStorage();
+  final baseUrl = dotenv.env['BASE_URL'].toString();
+  final getCreateProfile = dotenv.env['GET_CREATE_PROFILE_API'].toString();
+  final createInfo = {};
+
+  Future<void> getCreateProfileInfo() async {
+    try {
+      final data = jsonDecode(widget.data);
+      final accessToken = await storage.read(key: 'accessToken');
+      final url = Uri.parse('$baseUrl$getCreateProfile');
+      final headers = {'Authorization': 'Bearer $accessToken'};
+
+      final res = await http.get(url, headers: headers);
+      final status = res.statusCode;
+      print('${res.request} ==> $status');
+      print(res);
+
+      final info = jsonDecode(res.body);
+      if (status == 200) {
+        createInfo['nickname'] = info['nickname'];
+        createInfo['profileImage'] = info['profileImage'];
+
+        if (info['petProfileResponses'] &&
+            info['petProfileResponses'].length > 0) {
+          var petId = data['petId'];
+          var findItemIdx = info['petProfileResponses'].indexOf(petId);
+          createInfo['petName'] =
+              info['petProfileResponses'][findItemIdx]['petInfo']['name'];
+          createInfo['petProfileImage'] =
+              info['petProfileResponses'][findItemIdx]; // 수정 필요
+        } else {
+          createInfo['petName'] = '';
+          createInfo['petProfileImage'] = null;
+        }
+
+        // createInfo['petProfileImage'] = info['petProfileImage'];
+        print(createInfo);
+      } else {
+        print('fail');
+      }
+    } catch (err) {
+      print('err => $err');
+    }
+  }
+
   // 가입 완료 폭죽 효과
   bool isPlaying = false;
   late ConfettiController _controller;
@@ -21,6 +71,7 @@ class _RegisterFinishState extends State<RegisterFinish> {
     super.initState();
     _controller = ConfettiController(duration: const Duration(seconds: 5));
     _controller.play();
+    getCreateProfileInfo();
   }
 
   @override
@@ -50,37 +101,55 @@ class _RegisterFinishState extends State<RegisterFinish> {
               textAlign: TextAlign.center,
             ),
           ),
-          Align(
-            alignment: Alignment.topCenter,
-            child: Text(
-              '''글린다님 반가워요!
-오늘부터 카야와 
+          if (createInfo['nickname'] != null && createInfo['petName'] != null)
+            Align(
+              alignment: Alignment.topCenter,
+              child: Text(
+                '''${createInfo['nickname']}님 반가워요!
+오늘부터 ${createInfo['petName']}와 
 소중한 일상을 나눠주세요!''',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 18,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                ),
+                textAlign: TextAlign.center,
               ),
-              textAlign: TextAlign.center,
-            ),
-          ),
+            )
+          else
+            Text('XXXX'),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              SizedBox(
-                height: 100,
-                width: 100,
-                child: Image.asset('assets/user1.png'),
-              ),
+              createInfo['profileImage'] != null
+                  ? SizedBox(
+                      child: Image.network(
+                        '${createInfo['profileImage']}',
+                        width: 100,
+                        height: 100,
+                      ),
+                    )
+                  : SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: Image.asset('assets/user_default.png'),
+                    ),
               SizedBox(
                 height: 100,
                 width: 100,
                 child: Image.asset('assets/ic_heart.png'),
               ),
-              SizedBox(
-                height: 100,
-                width: 100,
-                child: Image.asset('assets/user2.png'),
-              ),
+              createInfo['petProfileImage'] != null
+                  ? SizedBox(
+                      child: Image.network(
+                        '${createInfo['petProfileImage']}',
+                        width: 100,
+                        height: 100,
+                      ),
+                    )
+                  : SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: Image.asset('assets/pet_default.png')),
             ],
           ),
           SizedBox(
@@ -90,14 +159,14 @@ class _RegisterFinishState extends State<RegisterFinish> {
               style: ButtonStyle(
                 shape: MaterialStateProperty.all(RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(15))),
-                backgroundColor:
-                MaterialStateProperty.all(Color(0xFFFFED8E)),
+                backgroundColor: MaterialStateProperty.all(Color(0xFFFFED8E)),
               ),
               onPressed: () => setState(
-                    () async {
+                () async {
                   // 홈 화면으로 이동
                   final result =
-                  await Navigator.of(context).push(_createRoute());
+                      await Navigator.of(context).push(_createRoute());
+                  // getCreateProfileInfo();
                 },
               ),
               child: Text(
@@ -116,21 +185,21 @@ class _RegisterFinishState extends State<RegisterFinish> {
 
   // 폭죽 효과 애니메이션
   Widget buildConfetti() => Align(
-    alignment: Alignment.center,
-    child: ConfettiWidget(
-      confettiController: _controller,
-      blastDirectionality: BlastDirectionality.explosive,
-      colors: [
-        Colors.red,
-        Colors.blue,
-        Colors.orange,
-        Colors.yellow,
-        Colors.pink,
-        Colors.green,
-      ],
-      shouldLoop: true,
-    ),
-  );
+        alignment: Alignment.center,
+        child: ConfettiWidget(
+          confettiController: _controller,
+          blastDirectionality: BlastDirectionality.explosive,
+          colors: [
+            Colors.red,
+            Colors.blue,
+            Colors.orange,
+            Colors.yellow,
+            Colors.pink,
+            Colors.green,
+          ],
+          shouldLoop: true,
+        ),
+      );
 
   // 페이지 전환 애니메이션
   Route _createRoute() {
@@ -142,7 +211,7 @@ class _RegisterFinishState extends State<RegisterFinish> {
         const curve = Curves.ease;
 
         var tween =
-        Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
 
         return SlideTransition(
           position: animation.drive(tween),
